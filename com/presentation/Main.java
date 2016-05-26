320fc5d12e21f2052f02b48f71c3834d257630f1
Amalric Lombard de Buffières <amalric.debuffieres@icloud.com>
2016-05-27 01:07:34 +0200

    Added user use from gui

--- com/presentation/Main.java
+++ com/presentation/Main.java
@@ -12,9 +12,9 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) {
 
-        launchOnConsole();
+        // launchOnConsole();
 
-        // launchOnGui();
+        launchOnGui();
 
     }
 
